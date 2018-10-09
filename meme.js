1cb1d7caff3572d95502bf95ca3124b2db276c31
LeightonSnyder <leighton.snyder@gmail.com>
2018-10-08 22:59:34 -0400

    added meme rough

--- meme.js
+++ meme.js
@@ -4,23 +4,29 @@ by Leighton
 mmp 210 wk 5
 rough
 */
-var bird; //global
+var cat1; //global
+var cat2;
 
 function preload(){
-    bird = loadImage("dove.jpg");
+    cat1 = loadImage("cat1.jpg");
+    cat2 = loadImage("cat2.jpg");
 }
     
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 533);
 }
 
 function draw() {
     background(80);
     
-    image(bird, 0, 0, width, height)
+    image(cat1, 0, 0, 400, 533);
+    image(cat2, 400, 0, 400, 533);
     
     fill(255);
     textAlign(CENTER, CENTER);
-    textSize(100);
-
+    textSize(20);
+    textFont("Trebuchet MS");
+    text("tuition free college", 200, 250, 150, 100);
+    text("GAMESHOW THAT PAYS OFF YOUR STUDENT DEBT", 575, 275, 200, 100);
+    
 }
