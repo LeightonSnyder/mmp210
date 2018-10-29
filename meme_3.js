7597c198374d3bf3114785a6b791cadd7c25f565
LeightonSnyder <leighton.snyder@gmail.com>
2018-10-28 23:18:54 -0400

    Finished animated meme game

--- meme_3.js
+++ meme_3.js
@@ -4,15 +4,23 @@ by Leighton
 mmp 210 wk 5
 Animated
 */
-var cat1; //global
+var cat1;
 var cat2;
 var frame;
 var cat;
 var speed = 25;
-var catX = 200;
-var catY = 50;
+var catX = 400;
+var catY = 250;
 var catspeed = 3;
-var toffY = 0;
+var Y1 = -10;
+var Y2 = -150;
+var Y3 = -250;
+var X1 = 400;
+var X2 = 200;
+var X3 = 600;
+var score = 0;
+
+
 
 
 function preload() {
@@ -20,17 +28,19 @@ function preload() {
     cat2 = loadImage("cat2.png");
     toffbar = loadImage("toffifay.png");
     toff = loadImage("toff.png");
+    backdrop = loadImage("background.jpg");
 }
 
 function setup() {
     createCanvas(800, 500);
     frame = 1;
-    
-
 }
 
 function draw() {
     background(80);
+    fill(255);
+    image(backdrop, 0, 0, 800, 600);
+
     //controls
     if (keyIsDown(UP_ARROW)){
         catY -= catspeed;
@@ -60,20 +70,67 @@ function draw() {
     }
     image(cat, catX-50, catY-50, 100, 100);
     
-    toffY++;
+    //candy movement
+    Y1++;
+    Y2++;
+    Y3++;
     
     push();
-    translate(width/2, toffY);
+    translate(X1, Y1);
     var r = frameCount / 75*PI;
     rotate(r);
     image(toffbar, -50, -11.34, 100, 22.68);
     pop();
-    translate(width/3, toffY);
+    
+    push();
+    translate(X2, Y2);
     rotate(r);
     image(toff, -8, -10, 16, 20);
-
-    //cat control
-    //catX += (keyleft + keyright);
-    //catY += (keyup + keydown);
-
-}
+    pop();
+    
+    push();
+    translate(X3, Y3);
+    rotate(r);
+    image(toff, -8, -10, 16, 20);
+    pop();
+    
+    //candy reset
+    if (Y1 > height) {
+        Y1 = 0;
+        X1 = random(2, width-2);
+    }
+    
+    if (Y2 > height) {
+        Y2 = 0;
+        X2 = random(2, width-2);
+    }
+    
+    if (Y3 > height) {
+        Y3 = 0;
+        X3 = random(2, width-2);
+    }
+    
+    //collisions
+    if ((dist(X1, Y1, catX, catY) <= 50))  {
+        Y1 = 0;
+        X1 = random(2, width-2);
+        score+=5;
+    }
+    if ((dist(X2, Y2, catX, catY) <= 50))  {
+        Y2 = 0;
+        X2 = random(2, width-2);
+        score++;
+    }
+    if ((dist(X3, Y3, catX, catY) <= 50))  {
+        Y3 = 0;
+        X3 = random(2, width-2);
+        score++;
+    }
+    //score text
+    strokeWeight(4);
+    stroke(0);
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    textFont("Trebuchet MS");
+    text("TOFFIFAY: "+score, 65, 20);
+   }
